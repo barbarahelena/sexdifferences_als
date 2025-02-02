2b872e7ed256ca2d6ee0d70eeb741f74b9b0bfbc
Verhaar <b828r@odcf-worker01.dkfz-heidelberg.de>
2025-02-02 22:12:26 +0100

    update script numbering

---
+++ scripts/1_5_boxplots.R
@@ -0,0 +1,84 @@
+## Boxplots metabolites
+## Barbara Verhaar, barbara.verhaar@dkfz-heidelberg.de
+
+## Libraries
+library(tidyverse)
+library(ggpubr)
+library(gridExtra)
+
+theme_Publication <- function(base_size=12, base_family="sans") {
+    library(grid)
+    library(ggthemes)
+    (theme_foundation(base_size=base_size, base_family=base_family)
+        + theme(plot.title = element_text(face = "bold",
+                                          size = rel(1.0), hjust = 0.5),
+                text = element_text(family = 'Helvetica'),
+                panel.background = element_rect(colour = NA),
+                plot.background = element_rect(colour = NA),
+                panel.border = element_rect(colour = NA),
+                axis.title = element_text(face = "bold",size = rel(0.9)),
+                axis.title.y = element_text(angle=90,vjust =2),
+                axis.title.x = element_text(vjust = -0.2),
+                axis.text.x = element_text(size = rel(0.9)),
+                axis.text = element_text(), 
+                axis.line = element_line(colour="black"),
+                axis.ticks = element_line(),
+                panel.grid.major = element_line(colour="#f0f0f0"),
+                panel.grid.minor = element_blank(),
+                legend.key = element_rect(colour = NA),
+                legend.position = "bottom",
+                # legend.direction = "horizontal",
+                legend.key.size= unit(0.2, "cm"),
+                legend.spacing  = unit(0, "cm"),
+                # legend.title = element_text(face="italic"),
+                plot.margin=unit(c(10,5,5,5),"mm"),
+                strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+                strip.text = element_text(face="bold")
+        ))
+    
+} 
+
+### Data ###
+df <- readRDS("data/metabolomics.RDS")
+allmets <- names(df)
+df$ID <- rownames(df)
+meta <- readRDS("data/metadata.RDS")
+df <- right_join(df, meta, by = "ID")
+tests <- rio::import("r_results/ttests/metabolites_welcht_diff.csv")
+tests2 <- rio::import("r_results/ttests/metabolites_welcht_ctrl_diff.csv")
+qval_sig <- unique(c(tests$metabolite[which(tests$q.value < 0.05)], tests2$metabolite[which(tests2$q.value < 0.05)]))
+pval_sig <- unique(c(tests$metabolite[which(tests$p.value < 0.05)], tests2$metabolite[which(tests2$p.value < 0.05)]))
+
+### Boxplot differences ###
+res_box <- list()
+for(a in 1:length(pval_sig)){
+    metab <- pval_sig[a]
+    metname <- paste0(pval_sig[a])
+    print(metname)
+    dfmet <- df %>% select(GroupPerSex, all_of(metname))
+    dfmet$met_y <- scale(dfmet[,2])
+    comp <- list(c("Female Control", "Male Control"), c("Female TDP43", "Male TDP43"))
+    (pl <- ggplot(data = dfmet, aes(x = GroupPerSex, y = met_y)) + 
+            ggpubr::stat_compare_means(method = "t.test", label = "p.signif", comparisons = comp,
+                                       hide.ns = TRUE, size = 5, #step.increase = 0.1,
+                                       var.equal = FALSE) +
+            geom_boxplot(aes(fill = GroupPerSex), outlier.shape = NA, 
+                         width = 0.5, alpha = 0.9) +
+            geom_jitter(color = "grey5", height = 0, width = 0.1, alpha = 0.75) +
+            scale_fill_manual(guide = "none", values = pal_nejm()(4)) +
+            ylim(NA, max(dfmet$met_y)*2) +
+            labs(title=metname, y="Metabolite (z-score)", x = "") +
+            theme_Publication() +
+            theme(axis.text.x = element_text(angle = 45, hjust = 1)))
+    ggsave(str_c("r_results/boxplots/", metname, ".pdf"), width = 4, height = 5.5, device = "pdf")
+    ggsave(str_c("r_results/boxplots/", metname, ".svg"), width = 4, height = 5.5, device = "svg")
+    res_box[[a]] <- pl
+}
+
+pdf("r_results/boxplots/boxplots_met.pdf", width = 15, height = 22)
+gridExtra::grid.arrange(grobs=res_box, ncol=5)
+dev.off()
+
+svg("r_results/boxplots/boxplots_met.svg", width = 15, height = 22)
+gridExtra::grid.arrange(grobs=res_box, ncol=5)
+dev.off()
---
+++ scripts/1_6_heatmap.R
@@ -0,0 +1,229 @@
+## Heatmap metabolites
+## Barbara Verhaar, barbara.verhaar@dkfz-heidelberg.de
+
+## Libraries
+library(tidyverse)
+library(ComplexHeatmap)
+library(circlize)
+library(rio)
+library(ggsci)
+
+## Data
+df <- readRDS("data/metadata.RDS")
+met <- readRDS("data/metabolomics.RDS")
+res1 <- rio::import("r_results/metabolites_welcht_ctrl_sig.csv")
+res2 <- rio::import("r_results/metabolites_welcht_sig.csv")
+totmetlist <- c(res1$metabolite, res2$metabolite)
+resfull1 <- rio::import("r_results/metabolites_welcht_ctrl_diff.csv")
+resfull2 <- rio::import("r_results/metabolites_welcht_diff.csv")
+totres <- full_join(resfull1 %>% filter(Sex == "Female" & metabolite %in% totmetlist) %>% select(1:4,7), 
+                    resfull2 %>% filter(Sex == "Female" & metabolite %in% totmetlist) %>% select(1:4,7), 
+                    by = "metabolite", suffix = c("_ctrl", "_tdp43"))
+totres
+met <- met %>% select(any_of(totmetlist))
+metanno <- met %>% mutate(ID = rownames(.)) %>% left_join(df, by = "ID") %>% arrange(Sex, Intervention)
+
+## Heatmap with all mice and metabolites with sex differences in ctrl/tdp43
+col_fun <- circlize::colorRamp2(c(-1, 0, 1), c(pal_nejm()(2)[2], "white", pal_nejm()(1))) # color mapping for blocks heatmap
+pvalue_col_fun = colorRamp2(c(0, 1, 3), c("lightgrey", "white", pal_nejm()(5)[5])) # color mapping for -log10(pvalue)
+matmet <- t(as.matrix(met)) # matrix of all mets (only works with matrices)
+mat <- as.data.frame(matmet)
+mat$metabolite <- rownames(mat)
+pvalanno <- left_join(mat, totres, by = "metabolite")
+matmet <- matmet[,metanno$ID]
+
+(pl1 <- Heatmap(matmet, name = "Z-score", 
+                col = col_fun, 
+                rect_gp = gpar(col = "white", lwd = 2),
+                column_title_gp = grid::gpar(fontsize = 8, fontface = "bold"),
+                column_split = c(rep("Ctrl (M)", 5), rep("TDP43 (M)", 5),
+                                    rep("Ctrl (F)", 5), rep("TDP43 (F)", 5)),  
+                column_order = metanno$ID,
+                cluster_columns = FALSE,
+                clustering_method_rows = "average",
+                top_annotation = HeatmapAnnotation(Group = anno_block(gp = gpar(fill = c(pal_nejm()(7)[c(3,4,6,7)]))), 
+                                                    height = unit(0.2, "cm")),
+                left_annotation = rowAnnotation(pvalue_tdp43 = anno_simple(-log10(pvalanno$p.value_tdp43), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_tdp43,
+                                                                     pt_size = unit(1, "snpc")*0.5
+                                                                     ),
+                                                pvalue_ctrl = anno_simple(-log10(pvalanno$p.value_ctrl), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_ctrl,
+                                                                     pt_size = unit(1, "snpc")*0.5
+                                                                     ),
+                                                annotation_name_side = "bottom",
+                                                width = unit(0.5, "cm"),
+                                                annotation_name_gp = grid::gpar(fontsize = 8)),
+                width = unit(6, "cm"), 
+                height = unit(0.25*28, "cm"), 
+                row_names_side = "left", 
+                column_gap = unit(2, "mm"),
+                row_dend_side = "right",
+                show_column_names = FALSE,
+                row_names_gp = grid::gpar(fontsize = 7))
+                )
+
+lgd_pvalue = Legend(title = "p-value", col_fun = pvalue_col_fun, at = c(0, 1, 2, 3), 
+                    labels = c("1", "0.1", "0.01", "0.001"))
+lgd_sig = Legend(pch = c("*","**","***","****"), type = "points", labels = c("0.05", "0.01", "0.001", "<0.001"))
+pl1_anno <- draw(pl1, annotation_legend_list = list(lgd_pvalue, lgd_sig))
+
+pdf("r_results/heatmaps/heatmap_fourgroups.pdf", width = 8, height = 10)
+pl1_anno
+dev.off()
+svg("r_results/heatmaps/heatmap_fourgroups.svg", width = 8, height = 10)
+pl1_anno
+dev.off()
+
+# Different color scheme
+col_fun <- circlize::colorRamp2(c(-1, 0, 1), c(pal_nejm()(6)[6], "white", pal_nejm()(3)[3])) # color mapping for blocks heatmap
+pvalue_col_fun = colorRamp2(c(0, 1, 3), c("lightgrey", "white", pal_nejm()(5)[5])) # color mapping for -log10(pvalue)
+matmet <- matmet[,order(metanno$ID)]
+
+(pl1 <- Heatmap(matmet, name = "Z-score", 
+                col = col_fun, 
+                rect_gp = gpar(col = "white", lwd = 2),
+                column_title_gp = grid::gpar(fontsize = 8, fontface = "bold"),
+                column_split = c(rep("Ctrl (M)", 5), rep("TDP43 (M)", 5),
+                                    rep("Ctrl (F)", 5), rep("TDP43 (F)", 5)),,
+                column_order = metanno$ID,
+                cluster_columns = FALSE,
+                clustering_method_rows = "average",
+                top_annotation = HeatmapAnnotation(
+                        Group = anno_block(gp = gpar(fill = c(pal_nejm()(7)[c(4,7,4,7)]),
+                                                        alpha = rep(0.9, 4))), 
+                        Sex = anno_block(gp = gpar(fill = c(pal_nejm()(2)[c(2,2,1,1)]),
+                                                        alpha = rep(0.9, 4))),
+                        height = unit(0.4, "cm")
+                        ),
+                left_annotation = rowAnnotation(pvalue_tdp43 = anno_simple(-log10(pvalanno$p.value_tdp43), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_tdp43,
+                                                                     pt_size = unit(1, "snpc")*0.7
+                                                                     ),
+                                                pvalue_ctrl = anno_simple(-log10(pvalanno$p.value_ctrl), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_ctrl,
+                                                                     pt_size = unit(1, "snpc")*0.7
+                                                                     ),
+                                                annotation_name_side = "bottom",
+                                                width = unit(0.8, "cm"),
+                                                annotation_name_gp = grid::gpar(fontsize = 8)),
+                width = unit(6, "cm"), 
+                height = unit(0.25*28, "cm"), 
+                row_names_side = "left", 
+                column_gap = unit(2, "mm"),
+                row_dend_side = "right",
+                show_column_names = FALSE,
+                row_names_gp = grid::gpar(fontsize = 7))
+                )
+                
+lgd_pvalue = Legend(title = "p-value", col_fun = pvalue_col_fun, at = c(0, 1, 2, 3), 
+                    labels = c("1", "0.1", "0.01", "0.001"))
+lgd_sex = Legend(title = "Sex", labels = c("Female", "Male"), 
+legend_gp = gpar(fill = c(rev(pal_nejm()(2)))))
+lgd_group = Legend(title = "Mice", labels = c("Control", "TDP43"), 
+legend_gp = gpar(fill = c(pal_nejm()(7)[c(4,7)])))
+lgd_sig = Legend(pch = c("*","**","***","****"), type = "points", 
+                    labels = c("0.05", "0.01", "0.001", "<0.001"), grid_width = unit(5, "mm"))
+pl1_anno <- draw(pl1, annotation_legend_list = list(lgd_pvalue, lgd_group, lgd_sex))
+
+pdf("r_results/heatmaps/heatmap_diffcolors.pdf", width = 8, height = 10)
+pl1_anno
+dev.off()
+svg("r_results/heatmaps/heatmap_diffcolors.svg", width = 8, height = 10)
+pl1_anno
+dev.off()
+
+(pl1 <- Heatmap(matmet, name = "Z-score", 
+                col = col_fun, 
+                rect_gp = gpar(col = "white", lwd = 2),
+                column_title_gp = grid::gpar(fontsize = 8, fontface = "bold"),
+                column_split = c(rep("Ctrl (M)", 5), rep("TDP43 (M)", 5),
+                                    rep("Ctrl (F)", 5), rep("TDP43 (F)", 5)),,
+                column_order = metanno$ID,
+                cluster_columns = FALSE,
+                clustering_method_rows = "average",
+                left_annotation = rowAnnotation(pvalue_tdp43 = anno_simple(-log10(pvalanno$p.value_tdp43), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_tdp43,
+                                                                     pt_size = unit(1, "snpc")*0.7
+                                                                     ),
+                                                pvalue_ctrl = anno_simple(-log10(pvalanno$p.value_ctrl), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_ctrl,
+                                                                     pt_size = unit(1, "snpc")*0.7
+                                                                     ),
+                                                annotation_name_side = "bottom",
+                                                width = unit(0.8, "cm"),
+                                                annotation_name_gp = grid::gpar(fontsize = 8)),
+                width = unit(6, "cm"), 
+                height = unit(0.25*28, "cm"), 
+                row_names_side = "left", 
+                column_gap = unit(2, "mm"),
+                row_dend_side = "right",
+                show_column_names = FALSE,
+                row_names_gp = grid::gpar(fontsize = 7))
+                )
+               
+lgd_pvalue = Legend(title = "p-value", col_fun = pvalue_col_fun, at = c(0, 1, 2, 3), 
+                    labels = c("1", "0.1", "0.01", "0.001"))
+lgd_sig = Legend(pch = c("*","**","***","****"), type = "points", 
+                    labels = c("0.05", "0.01", "0.001", "<0.001"),
+                    legend_gp = gpar(fontsize = 8))
+pl1_anno <- draw(pl1, annotation_legend_list = list(lgd_pvalue, lgd_sig))
+
+pdf("r_results/heatmaps/heatmap_noanno.pdf", width = 8, height = 10)
+pl1_anno
+dev.off()
+svg("r_results/heatmaps/heatmap_noanno.svg", width = 8, height = 10)
+pl1_anno
+dev.off()
+
+## Heatmap with means per group and metabolites with sex differences in ctrl/tdp43
+totres2 <- full_join(resfull1, resfull2, 
+                    by = c("metabolite", "Sex"), suffix = c("_ctrl", "_tdp43")) %>% 
+                    pivot_wider(., names_from = "Sex", 
+                        values_from = c("mean_ctrl", "p.value_ctrl", "mean_tdp43", "p.value_tdp43"))
+rownames(totres2) <- totres2$metabolite
+totres2 <- totres %>% select(contains("mean"), contains("p.value"))
+(pl1 <- Heatmap(matmet, name = "Z-score", 
+                col = col_fun, 
+                rect_gp = gpar(col = "white", lwd = 2),
+                column_title_gp = grid::gpar(fontsize = 8, fontface = "bold"),
+                column_split = c(rep("Ctrl (M)", 5), rep("TDP43 (M)", 5),
+                                    rep("Ctrl (F)", 5), rep("TDP43 (F)", 5)),,
+                column_order = metanno$ID,
+                cluster_columns = FALSE,
+                clustering_method_rows = "average",
+                left_annotation = rowAnnotation(pvalue_tdp43 = anno_simple(-log10(pvalanno$p.value_tdp43), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_tdp43,
+                                                                     pt_size = unit(1, "snpc")*0.7
+                                                                     ),
+                                                pvalue_ctrl = anno_simple(-log10(pvalanno$p.value_ctrl), 
+                                                                     which = 'row',
+                                                                     col = pvalue_col_fun, 
+                                                                     pch = pvalanno$sig_ctrl,
+                                                                     pt_size = unit(1, "snpc")*0.7
+                                                                     ),
+                                                annotation_name_side = "bottom",
+                                                width = unit(0.8, "cm"),
+                                                annotation_name_gp = grid::gpar(fontsize = 8)),
+                width = unit(6, "cm"), 
+                height = unit(0.25*28, "cm"), 
+                row_names_side = "left", 
+                column_gap = unit(2, "mm"),
+                row_dend_side = "right",
+                show_column_names = FALSE,
+                row_names_gp = grid::gpar(fontsize = 7))
+                )
