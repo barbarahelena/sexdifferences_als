1407a6b06070e54e4370e1704b8ba9cb3210b12b
barbarahelena <bjh.verhaar@gmail.com>
2025-02-21 12:36:06 +0100

    increase threshold pruning, add test stratified pathway analysis

--- scripts/3_1_clean_pathways.R
+++ scripts/3_1_clean_pathways.R
@@ -8,18 +8,39 @@ library(forcats)
 
 # Tidy pathway table
 pathw <- rio::import("data/pathway_abundance_cpm_unstratified.txt") %>% 
-                filter(`# Pathway` != "UNMAPPED" & `# Pathway` != "UNINTEGRATED")
+                filter(`# Pathway` != "UNMAPPED" & `# Pathway` != "UNINTEGRATED") %>%
+                select(`# Pathway`, contains("L")) %>%
+                rename_with(~ str_remove(., "_Abundance"), 1:ncol(.))
 head(pathw)
 names(pathw)
 paths <- pathw$`# Pathway`
 keys <- str_extract(paths, "[A-Z0-9-]+(?=:)")
 expl <- trimws(str_extract(paths, "(?<=:).*"))
 lib <- data.frame(paths, keys, expl)
-pathw <- pathw %>% rename_with(~ str_remove(., "_Abundance"), 2:ncol(.)) %>% 
-                    mutate(`# Pathway` = str_extract(`# Pathway`, "[A-Z0-9-]+(?=:)"))
+pathw <- pathw %>% mutate(`# Pathway` = str_extract(`# Pathway`, "[A-Z0-9-]+(?=:)"))
 rownames(pathw) <- pathw$`# Pathway`
 pathw$`# Pathway` <- NULL
 pathdf <- as.data.frame(t(as.matrix(pathw)))
+
+# Tidy stratified pathway table
+# pathw <- rio::import("data/pathway_abundance_cpm_stratified.txt") %>%
+#                 filter(!(str_detect(`# Pathway`, "UNMAPPED")) & !(str_detect(`# Pathway`, "UNINTEGRATED"))) %>%
+#                 select(contains("L"), `# Pathway`) %>%
+#                 rename(pathway = `# Pathway`) %>%
+#                 rename_all(~ str_remove(., "_Abundance"))
+# head(pathw)
+# names(pathw)
+# load <- read.csv("results/microbiome/tcam/pythonoutput/df_loadings.csv")
+# f1 <- load %>% select(X, F1.19.99.) %>% arrange(-F1.19.99.) %>% slice(1:10, min10:last)
+# f1$X <- str_replace_all(f1$X, " ", "_")
+# res <- c()
+# for (i in f1$X){
+#     pathfiltered <- pathw %>% filter(str_detect(pathway, i))
+#     res <- rbind(res, pathfiltered)
+# }
+# res <- as.data.frame(res)
+# head(res)
+# dim(res) # Ok so this only works for Akkermansia.. skip
 
 # Explore
 head(pathdf)[1:5,1:5]
@@ -32,12 +53,12 @@ summary(means < 0.1)
 medians <- sapply(pathdf, median)
 summary(medians < 40)
 zero_counts <- apply(pw, 2, function(x) sum(x == 0))
-summary(zero_counts > (0.8*140))
-pw <- pw[,zero_counts<(0.8*140)] # 59 pathways removed
+summary(zero_counts > (0.5*140))
+pw <- pw[,zero_counts<(0.5*140)] # 105 pathways removed
 
 # Log-transform the pathways
 pw <- log10(pw + 1)
-dim(pw)
+dim(pw) # 108 pathways
 head(pw)
 
 saveRDS(pw, "data/pathways.RDS")
