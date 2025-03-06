a6ca34adf8aa12edb6e2e58ae2bb6d1a595a2914
barbarahelena <bjh.verhaar@gmail.com>
2025-03-06 12:08:04 +0100

    update filtering pathways

--- scripts/3_1_clean_pathways.R
+++ scripts/3_1_clean_pathways.R
@@ -11,6 +11,7 @@ pathw <- rio::import("data/pathway_abundance_cpm_unstratified.txt") %>%
                 filter(`# Pathway` != "UNMAPPED" & `# Pathway` != "UNINTEGRATED") %>%
                 select(`# Pathway`, contains("L")) %>%
                 rename_with(~ str_remove(., "_Abundance"), 1:ncol(.))
+
 head(pathw)
 names(pathw)
 paths <- pathw$`# Pathway`
@@ -22,43 +23,15 @@ rownames(pathw) <- pathw$`# Pathway`
 pathw$`# Pathway` <- NULL
 pathdf <- as.data.frame(t(as.matrix(pathw)))
 
-# Tidy stratified pathway table
-# pathw <- rio::import("data/pathway_abundance_cpm_stratified.txt") %>%
-#                 filter(!(str_detect(`# Pathway`, "UNMAPPED")) & !(str_detect(`# Pathway`, "UNINTEGRATED"))) %>%
-#                 select(contains("L"), `# Pathway`) %>%
-#                 rename(pathway = `# Pathway`) %>%
-#                 rename_all(~ str_remove(., "_Abundance"))
-# head(pathw)
-# names(pathw)
-# load <- read.csv("results/microbiome/tcam/pythonoutput/df_loadings.csv")
-# f1 <- load %>% select(X, F1.19.99.) %>% arrange(-F1.19.99.) %>% slice(1:10, min10:last)
-# f1$X <- str_replace_all(f1$X, " ", "_")
-# res <- c()
-# for (i in f1$X){
-#     pathfiltered <- pathw %>% filter(str_detect(pathway, i))
-#     res <- rbind(res, pathfiltered)
-# }
-# res <- as.data.frame(res)
-# head(res)
-# dim(res) # Ok so this only works for Akkermansia.. skip
+# Filter
+pathway_filter <- apply(pathdf, 2, function(x) sum(x > 5) >= (0.5 * nrow(pathdf)))
+pw_filtered <- pathdf[, pathway_filter]
 
-# Explore
-head(pathdf)[1:5,1:5]
-dim(pathdf) # 234 pathways
-variances <- sapply(pathdf, sd)
-summary(variances < 5)
-pw <- pathdf[,variances > 5] # 21 pathways removed
-means <- sapply(pathdf, mean)
-summary(means < 0.1)
-medians <- sapply(pathdf, median)
-summary(medians < 40)
-zero_counts <- apply(pw, 2, function(x) sum(x == 0))
-summary(zero_counts > (0.5*140))
-pw <- pw[,zero_counts<(0.5*140)] # 105 pathways removed
+# Log transform filtered pathways
+pw <- log10(pw_filtered + 1)
 
-# Log-transform the pathways
-pw <- log10(pw + 1)
-dim(pw) # 108 pathways
+# Print dimensions
+dim(pw)
 head(pw)
 
 saveRDS(pw, "data/pathways.RDS")
