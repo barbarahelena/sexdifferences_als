d781435827d6114a5e4cbdf07f5f4038488f0d46
Barbara Verhaar <barbaraverhaar@MacBook-Pro-van-Barbara.local>
2026-02-22 14:36:23 +0100

    update email and theme

--- scripts/2_5_tcam_prep.R
+++ scripts/2_5_tcam_prep.R
@@ -1,5 +1,5 @@
 # Impute microbiome data
-# Barbara Verhaar, barbara.verhaar@dkfz-heidelberg.de
+# Barbara Verhaar, b.j.verhaar@amsterdamumc.nl
 
 ## Load libraries
 library(tidyverse)
@@ -12,8 +12,8 @@ theme_Publication <- function(base_size=14, base_family="sans") {
         + theme(plot.title = element_text(face = "bold",
                                           size = rel(1.0), hjust = 0.5),
                 text = element_text(),
-                panel.background = element_rect(colour = NA),
-                plot.background = element_rect(colour = NA),
+                panel.background = element_rect(colour = NA, fill = NA),
+                plot.background = element_rect(colour = NA, fill = NA),
                 panel.border = element_rect(colour = NA),
                 axis.title = element_text(face = "bold",size = rel(0.8)),
                 axis.title.y = element_text(angle=90, vjust =2),
